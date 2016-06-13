84b45fd0ea3e96eac1afd6425a1bd7d7c2ef3177
Niko Kouloufakos <nikolas.kouloufakos@shopify.com>
2016-06-13 13:02:41 +0000

    Add a full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,20 +7,26 @@ end
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
-    assert_response :successass
-    assert_select "title", "Home | #{@base_title}"
+    assert_response :success
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "#{@base_title}"
+  end 
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "#{@base_title}"
   end 
 
 end
